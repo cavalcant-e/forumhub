f4dcc2dc50744db2b1e81b107549549090dc2118
cavalcant-e <jonnathy@hotmail.com>
2024-07-03 15:10:47 -0300

    first commit

---
+++ ForumHub/src/test/java/Cavalcante/com/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package Cavalcante.com.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
